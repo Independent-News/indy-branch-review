00ba3135cc376165b831ff3f318156843f89cc67
Jenkins <jenkins@independent.co.uk>
2024-10-11 09:52:57 +0000

    Deploying review artefacts for INDY-1927/fix/update-twitter-assets-with-new-x-part-2

---
+++ UI-Storybook/src/components/Form/ValidationHint/ValidationHint.d.ts
@@ -0,0 +1,5 @@
+import { default as React } from 'react';
+import { ValidationHintProps } from './ValidationHint.types';
+declare const ValidationHint: React.FC<ValidationHintProps>;
+export default ValidationHint;
+//# sourceMappingURL=ValidationHint.d.ts.map
